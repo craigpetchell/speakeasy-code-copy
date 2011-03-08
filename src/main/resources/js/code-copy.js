14f553f576756fd2ed52c3adf80282b366f74ffb
Craig Petchell <craig@petchell.org>
2011-03-08 13:41:04 +1100

    Fix issues running in Confluence

---
+++ src/main/resources/js/code-copy.js
@@ -0,0 +1,37 @@
+AJS.toInit(function($) {
+    var clipboardMovie = AJS.Data.get("static-resource-url-prefix") +
+            "/download/batch/org.petchell.speakeasy.codecopy:code-copy-1/org.petchell.speakeasy.codecopy:zero-clipboard.swf";
+    AJS.log("clipboardMovie = " + clipboardMovie);
+    ZeroClipboard.setMoviePath(clipboardMovie);
+    $('.codeContent').each(function() {
+        var contentContainer = $(this);
+        var text = contentContainer.text();
+        var copyContainer = $('<div></div>').addClass('copy-container').appendTo(contentContainer);
+        var copyButton = $('<div></div>').addClass('copy-button').appendTo(copyContainer);
+
+        var clip = new ZeroClipboard.Client();
+        clip.set
+        clip.setHandCursor( true );
+
+        clip.addEventListener('load', function (client) {
+            console.log("Flash movie loaded and ready.");
+        });
+
+        clip.addEventListener('mouseover', function (client) {
+            copyButton.fadeTo('fast', 1);
+        });
+
+        clip.addEventListener('mouseout', function (client) {
+            copyButton.fadeTo('fast', 0.5);
+        });
+
+        clip.addEventListener('complete', function (client, text) {
+            console.log("Copied text to clipboard: " + text );
+        });
+
+        clip.glue(copyButton[0], copyContainer[0]);
+        clip.setText(text);
+    });
+
+
+});
